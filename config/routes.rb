d7eb7cb6721e6108009e13bcc3284e0ce83aebd2
Nicoer <saber00sword@gmail.com>
2017-11-24 19:16:19 +0800

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  resources :microposts
-  resources :m_icroposts
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
 end
